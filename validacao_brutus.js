248cab3f8ec7f27f2f582fedb06447a039bc9bb4
Ludmila Machado <myllamachaado@gmail.com>
2020-12-19 15:10:40 -0300

    Update validacao_brutus.js

--- validacao_brutus.js
+++ validacao_brutus.js
@@ -4,8 +4,7 @@ var id_venda_brutus = pm.variables.get('id_venda_brutus');
 var id_venda_brutus_delivered = pm.variables.get('id_venda_brutus_delivered');
 var status_venda_brutus_delivered = pm.variables.get('status_venda_brutus_delivered');
 
-    if((pm.expect(id_venda_brutus).to.eql(id_venda_brutus_delivered))&&
-        (pm.expect(status_venda_brutus_delivered).to.eql('delivered'))){
+    if((pm.expect(id_venda_brutus).to.eql(id_venda_brutus_delivered))&&(pm.expect(status_venda_brutus_delivered).to.eql('delivered'))){
         console.log('Status da venda trocado com sucesso');
     }
 }
